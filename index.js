0fac982da591a8c0dd9219dbeff85c29cac5d895
inuscript <inuscript@gmail.com>
2017-01-17 12:58:00 +0900

    refactor

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 const postcss = require('postcss');
 
+const createRule = (selector, prefix) => {
+    const node = postcss.rule({
+        selector: `${selector}::--ms-${prefix}`
+    });
+    node.append(postcss.decl({
+        prop: 'display',
+        value: 'none'
+    }));
+    return node;
+
+};
 module.exports = postcss.plugin('postcss-ie-appearance-none', function () {
     // Work with options here
 
@@ -10,15 +21,7 @@ module.exports = postcss.plugin('postcss-ie-appearance-none', function () {
             if (decl.value !== 'none') { // skip
                 return;
             }
-            const selector = decl.parent.selector;
-            const node = postcss.rule({
-                selector: `${selector}::--ms-expand`
-            });
-            node.append(postcss.decl({
-                prop: 'display',
-                value: 'none'
-            }));
-
+            const node = createRule(decl.parent.selector, 'expand');
             root.insertAfter(decl.parent, node);
         });
 
